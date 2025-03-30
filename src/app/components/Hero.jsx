b35753fe113397c6627d6b40e5e8bf3696b70583
Albin Joby <albinjobyponnu@gmail.com>
2025-03-30 19:32:33 +0530

    fix

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -84,8 +84,8 @@ const Hero = () => {
   const getVideoSource = (index) => `videos/hero-${index}.mp4`;
 
 return (
-    <div className="relative h-dvh w-screen overflow-x-hidden">
-            {/* {isLoading && (
+  <div className="relative h-dvh w-screen overflow-x-hidden">
+    {/* {isLoading && (
                     <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-white">
                             <div className="three-body">
                             <div className="three-body__dot" />
@@ -94,72 +94,74 @@ return (
                             </div>
                     </div>
             )} */}
-        <div
-            id="video-frame"
-            className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
-        >
-            <div>
-                <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-                    <div
-                        onClick={handleMiniVdClick}
-                        className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-                    >
-                        <video
-                            ref={nextVideoRef}
-                            src={getVideoSource(upcommingVideoIndex)}
-                            loop
-                            muted
-                            id="current-video"
-                            className="size-64 origin-center scale-150 object-cover object-center"
-                            onLoadedData={handleVideoLoad}
-                        />
-                    </div>
-                </div>
-                <video
-                    ref={nextVideoRef}
-                    src={getVideoSource(currentIndex)}
-                    loop
-                    muted
-                    id="next-video"
-                    className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
-                    onLoadedData={handleVideoLoad}
-                />
-                <video
-                    src={getVideoSource(
-                        currentIndex === totalVideos - 1 ? 1 : currentIndex
-                    )}
-                    autoPlay
-                    loop
-                    muted
-                    onLoadedData={handleVideoLoad}
-                    className="absolute left-0 top-0 size-full object-cover object-center"
-                />
-            </div>
-            <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-                G<b>a</b>ming
-            </h1>
-            <div className="absolute left-0 top-0 z-40 size-full">
-                <div className="mt-24 px-5 sm:px-10">
-                    <h1 className="special-font hero-heading text-blue-100">
-                        redefi<b>n</b>e
-                    </h1>
-                    <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-                        Enter teh Metagame Layer <br />
-                        Unleash teh Play Economy
-                    </p>
-                    <Button
-                        id="watch-trailer"
-                        title="Watch Trailer"
-                        leftIcon={<TiLocationArrow />}
-                        containerClass="!bg-yellow-300 flex-center gap-1"
-                    />
-                </div>
-            </div>
+    <div
+      id="video-frame"
+      className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+    >
+      <div>
+        <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+          <div
+            onClick={handleMiniVdClick}
+            className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+          >
+            <video
+              ref={nextVideoRef}
+              src={getVideoSource(upcommingVideoIndex)}
+              loop
+              muted
+              id="current-video"
+              className="size-64 origin-center scale-150 object-cover object-center"
+              onLoadedData={handleVideoLoad}
+            />
+          </div>
         </div>
-        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-            G<b>a</b>ming
-        </h1>
+        <video
+          ref={nextVideoRef}
+          src={getVideoSource(currentIndex)}
+          loop
+          muted
+          id="next-video"
+          className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+          onLoadedData={handleVideoLoad}
+        />
+        <video
+          src={getVideoSource(
+            currentIndex === totalVideos - 1 ? 1 : currentIndex
+          )}
+          autoPlay
+          playsInline
+          webkit-playsinline="true"
+          loop
+          muted
+          onLoadedData={handleVideoLoad}
+          className="absolute left-0 top-0 size-full object-cover object-center"
+        />
+      </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+        G<b>a</b>ming
+      </h1>
+      <div className="absolute left-0 top-0 z-40 size-full">
+        <div className="mt-24 px-5 sm:px-10">
+          <h1 className="special-font hero-heading text-blue-100">
+            redefi<b>n</b>e
+          </h1>
+          <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+            Enter the Metagame Layer <br />
+            Unleash teh Play Economy
+          </p>
+          <Button
+            id="watch-trailer"
+            title="Watch Trailer"
+            leftIcon={<TiLocationArrow />}
+            containerClass="!bg-yellow-300 flex-center gap-1"
+          />
+        </div>
+      </div>
     </div>
+    <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+      G<b>a</b>ming
+    </h1>
+  </div>
 );
 };
 
